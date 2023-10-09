903547558402ba8835b1ef9928c54ec2c6956007
mikumifa <1055069518@qq.com>
2023-10-10 00:03:51 +0800

    导航栏, 基本的登录功能, 加密工具类

---
+++ ChatDiary/app/src/main/java/com/example/chatdiary/ui/chat/ChatViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.chatdiary.ui.chat
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is chat Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ ChatDiary/app/src/main/java/com/example/chatdiary/ui/diary/DiaryFragment.kt
@@ -0,0 +1,42 @@
+package com.example.chatdiary.ui.diary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.chatdiary.databinding.FragmentDiaryBinding
+
+class DiaryFragment : Fragment() {
+
+    private var _binding: FragmentDiaryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val diaryViewModel =
+            ViewModelProvider(this).get(DiaryViewModel::class.java)
+
+        _binding = FragmentDiaryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDiary
+        diaryViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
