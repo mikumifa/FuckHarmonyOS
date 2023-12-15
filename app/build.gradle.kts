08dd709ef1d79dad8a75b410b3efedc7cf567411
mikumifa <1055069518@qq.com>
2023-12-16 00:39:23 +0800

    feat: 添加聊天时候的图片

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "1.1.4"
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
